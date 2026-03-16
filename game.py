c95f023e4172068d0faef040b0519b38fc44b986
Parth Mohan <parth.mohan.india@gmail.com>
2026-03-17 05:28:09 +0800

    Debugging
    Editing files and debugging the game.py file

--- game.py
+++ game.py
@@ -15,8 +15,7 @@ class Game: # We make the Game code into a class of its own to be called in the 
 
         pygame.display.set_caption("Ninja Game") # This line of code sets the title of the game as Ninja Game
         self.screen = pygame.display.set_mode((640, 480)) # This line sets the display for the gmae as 640,480 pixels
-
-        self.screen = pygame.Surface((320,240)) 
+        self.display = pygame.Surface((320,240)) 
 
         self.clock = pygame.time.Clock()
 
@@ -27,13 +26,10 @@ class Game: # We make the Game code into a class of its own to be called in the 
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'), 
             'stone': load_images('tiles/stone'),
-            'spawners': load_images('tiles/spawners'),
             'player': load_image('entities/player.png'), # Load the player image using the load_image function from utils.py
             'background': load_image('background.png'),
             'clouds': load_images('clouds'), # Load the cloud images using the load_images function from utils.py
         }
-
-        print(self.assets) # This line prints the assets dictionary to the console to check if the images are loaded correctly.
 
         self.clouds = Clouds(self.assets['clouds'], count = 16) # create a Clouds instance with the loaded cloud images
 
@@ -46,19 +42,19 @@ class Game: # We make the Game code into a class of its own to be called in the 
 
     def run(self): # This function makes the loop in its own function which can be called in the future
         while True:
-            self.screen.blit(self.assets['background'], (0, 0)) # Draw the background image onto the screen at position (0, 0)
+            self.display.blit(self.assets['background'], (0, 0)) # Draw the background image onto the screen at position (0, 0)
 
-            self.scroll[0] +=(self.player.rect().centerx - self.screen.get_width() / 2 - self.scroll[0]) / 30 # The self.screen.get_width part of the code essentially cuts the screen's size by half so that the camera ends up tracking the player and not somewhere else. update horizontal scroll based on player position. As the camera is linked to the player's position
-            self.scroll[1] +=(self.player.rect().centery - self.screen.get_height() / 2 - self.scroll[1]) / 30 # update vertical scroll based on player position
+            self.scroll[0] +=(self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30 # The self.display.get_width part of the code essentially cuts the display's size by half so that the camera ends up tracking the player and not somewhere else. update horizontal scroll based on player position. As the camera is linked to the player's position
+            self.scroll[1] +=(self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30 # update vertical scroll based on player position
             render_scroll = (int(self.scroll[0]), int(self.scroll[1])) # create a tuple of integer scroll values for rendering. This is a key line of code as if we run the game without this code, the player gets jittery as the player's position and tiles position are float values not integer values.
 
             self.clouds.update() # update the clouds' positions
-            self.clouds.render(self.screen, offset = render_scroll) # render the clouds onto the
+            self.clouds.render(self.display, offset = render_scroll) # render the clouds onto the
 
-            self.tilemap.render(self.screen, offset = self.scroll) # render the tilemap onto the screen with the current scroll offset. The scroll offset is the camera position in the game world. 
+            self.tilemap.render(self.display, offset = self.scroll) # render the tilemap onto the display with the current scroll offset. The scroll offset is the camera position in the game world. 
 
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))  # update player with vertical movement input
-            self.player.render(self.screen, offset = self.scroll)  # render player with current scroll offset
+            self.player.render(self.display, offset = self.scroll)  # render player with current scroll offset
            
             
             for event in pygame.event.get():
