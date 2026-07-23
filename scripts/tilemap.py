208d7faccd689ecbbd77d1a796b019b3b16c3d0b
Parth Mohan <parth.mohan.india@gmail.com>
2026-07-23 20:32:29 +0800

    Developing game

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -34,9 +34,13 @@ class Tilemap:
          return rects
 
 
-    def render(self,surf, offset=(0,0)): # Render the tilemap onto the provided surface with an optional 
-        for tile in self.offgrid_tiles:  
-            surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] - offset[0], tile['pos'][1] - offset[1])) # Renders offgrid tiles at their pixel positions 
+    def render(self,surf, offset=(0,0)):
+        for x in range(offset[0] // self.tile_size, (offset[0] + surf.get_width()) // self.tile_size + 1):
+            pass  # Loop through the range of tile coordinates that are visible on the surface based on the offset and surface width
+        
+         # Render the tilemap onto the provided surface with an optional 
+        # for tile in self.offgrid_tiles:  
+            # surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] - offset[0], tile['pos'][1] - offset[1])) # Renders offgrid tiles at their pixel positions 
         
         
         for x in range(offset[0] // self.tile_size, (offset[0] + surf.get_width()) // self.tile_size + 1): # Loop through the range of tile coordinates that are visible on the surface based on the offset and surface width
